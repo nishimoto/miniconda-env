85449ec0683f17df35e952cbac5133f9fd4016e6
nishimoto <thinkn1108@gmail.com>
2019-05-03 17:38:18 +0900

    Delete scikit-bio

--- miniconda.sh
+++ miniconda.sh
@@ -1,24 +1,10 @@
-# 1. download miniconda
-# https://docs.conda.io/en/latest/miniconda.html
+#!/bin/bash
+# Download from default channel
+conda install -y matplotlib seaborn numpy scipy scikit-learn pandas lightgbm tqdm ipython jupyter
 
-# 2. bash
-bash Miniconda3-latest-MacOSX-x86_64.sh
+# Download from non-default channel
+conda install -y -c conda-forge xgboost
+conda install -y -c anaconda scikit-bio
 
-# 3. Do with installer
-
-# 4. activate conda settings
-source .bash_profile
-
-# 5. Install python libraries
-## From default channel
-conda install matplotlib seaborn numpy scipy scikit-learn pandas lightgbm tqdm ipython jupyter
-
-## Download from non-default channel
-conda install -c conda-forge xgboost
-conda install -c anaconda scikit-bio
-
-## pyper is in conda, but we cant download if you use MacOS
+# pyper is in conda, but I cant download if you use MacOS
 pip install pyper
-
-## As scikit-bio update some of libraries, you should install at last.
-conda install -y -c https://conda.anaconda.org/biocore scikit-bio
