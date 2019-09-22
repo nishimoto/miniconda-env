7befb67bab6632d08c1e193f8af4d2812a5dbdc4
nishimoto <thinkn1108@gmail.com>
2019-09-22 19:29:19 +0900

    Update miniconda.sh

--- miniconda.sh
+++ miniconda.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Download from default channel
 conda install -y matplotlib seaborn numpy scipy scikit-learn pandas lightgbm tqdm ipython jupyter openpyxl xlrd
-conda install -y -c conda-forge xgboost
 
-# 使わないかも
-conda install -y -c anaconda scikit-bio
-
-# pyper is in conda, but I cant download if you use MacOS
-pip install pyper
+# Download from non-default channel.
+conda install -y -c conda-forge xgboost catboost
